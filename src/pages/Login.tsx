5fcffe79b4316a64f6bbcb0cd936723c9ee6e727
Vargas Vasquez Emmanuel Ariel <emma.vargasvasquez@gmail.com>
2025-10-29 00:07:08 -0300

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { AlertCircle, Eye, EyeOff, LogIn } from "lucide-react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router";
 import { useAuthStore } from "../stores/authStore";
 
 const Login: React.FC = () => {
