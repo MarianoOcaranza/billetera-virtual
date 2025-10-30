541b6bcf2af6f0f4402bd37a1683aa7c6de6bc93
Mariano Ocaranza <marianoocaranza@gmail.com>
2025-10-30 15:57:18 -0300

    comprobante test en produccion

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import fs from 'fs'
-import path from 'path'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(), 
     tailwindcss()
   ],
-  server: {
-    host: 'localhost',
-    port: 5173,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
-    },
-  }
 })
